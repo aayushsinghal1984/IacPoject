d840482284f894cdf7e737af09ea53298df33a6c
aayushsinghal1984 <aayush.singhal01@gmail.com>
2026-06-19 01:53:25 +0530

    Update main.tf

--- network/env/main.tf
+++ network/env/main.tf
@@ -1,26 +1,26 @@
 module "RG" {
-  source              = "../module/resourcegroup"
+  source              = "./module/resourcegroup"
   rg                  = var.resource_group
 }
 module "vnets" {
   depends_on          = [module.RG]
-  source              = "../module/virtualnetwork"
+  source              = "./module/virtualnetwork"
   vnets               = var.vnets
   
 }
 module "subnets" {
   depends_on          = [module.RG , module.vnets]
-  source              = "../module/subnet"
+  source              = "./module/subnet"
   subnets             = var.subnets
 }
 
 module "nsg" {
   depends_on          = [module.RG]
-  source              = "../module/nsg"
+  source              = "./module/nsg"
   NSG                 = var.NSG
 }
 module "nsgsubnetassociation" {
   depends_on          = [module.subnets , module.nsg]
-  source              = "../module/nsgsubnetassociation"
+  source              = "./module/nsgsubnetassociation"
   nsgsubnetassociation = var.nsgsubnetassociation
 }
