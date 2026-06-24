18066be0e8f44ac37adc265b293d896e832e4d48
aayushsinghal1984 <aayush.singhal01@gmail.com>
2026-06-24 15:07:22 +0000

    Update provider.tf

--- terraform-bootstrap/provider.tf
+++ terraform-bootstrap/provider.tf
@@ -1,10 +1,7 @@
 terraform {
-    required_version = ">= 1.5.0"
-
-  required_providers {
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "~> 0.50"
+  cloud {
+    organization = var.organization
+    workspaces {   
     }
   }
 }
