985fedf063918229cefa5b2268ee0953b84f2f0a
aayushsinghal1984 <aayushsinghal1984@gmail.com>
2026-07-17 09:24:52 +0000

    add

--- network/env/provider.tf
+++ network/env/provider.tf
@@ -1,13 +1,5 @@
 terraform {
-  required_version = ">= 1.5.0"
-
-  cloud {
-    organization = "HCLIT"
-
-    workspaces {
-  
-    }
-  }
+  required_version = ">= 1.13.0"
 
   required_providers {
     azurerm = {
